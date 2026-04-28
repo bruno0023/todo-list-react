32e61e1e1ad00b4f1222ded2c4c7ac476011cd63
Bruno Alves de freitas <brunoalves-al@outlook.com>
2026-04-28 09:43:40 -0300

    feat: filtro das tasks finalizado

--- src/core-components/task-filter.tsx
+++ src/core-components/task-filter.tsx
@@ -4,21 +4,45 @@ import Button from "../components/button";
 //Hooks
 import useTaks from "../hooks/useTasks";
 
-export default function TaskFilter() {
+export type filterProp = 'all' | 'pending' | 'completed';
+
+export type TaskFilterProps = {
+    filter: filterProp
+    setFilter: (value: filterProp) => void
+}
+
+export default function TaskFilter({
+    filter,
+    setFilter
+}: TaskFilterProps) {
 
     const { TasksInfo } = useTaks()
 
+
+    function handleSetTaskFilter(state: filterProp) {
+        setFilter(state)
+    }
+
     return <>
         <div className="flex justify-between gap-4 p-2">
-            <Button variant="secondary" className="w-full gap-2">
+            <Button
+                onClick={() => handleSetTaskFilter('all')}
+                variant={filter === 'all' ? 'active' : 'secondary'}
+                className="w-full gap-2">
                 <p>Todas</p>
                 <Badge value={TasksInfo.count} />
             </Button>
-            <Button variant="secondary" className="w-full gap-2">
+            <Button
+                onClick={() => handleSetTaskFilter('pending')}
+                variant={filter === 'pending' ? 'active' : 'secondary'}
+                className="w-full gap-2">
                 <p>Pendentes</p>
                 <Badge value={TasksInfo.countPending} />
             </Button>
-            <Button variant="secondary" className="w-full gap-2">
+            <Button
+                onClick={() => handleSetTaskFilter('completed')}
+                variant={filter === 'completed' ? 'active' : 'secondary'}
+                className="w-full gap-2">
                 <p>Concluidas</p>
                 <Badge value={TasksInfo.countConclued} />
             </Button>
--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,15 +1,30 @@
 
+import { useState } from "react";
 import Search from "../core-components/search";
 import TaskFilter from "../core-components/task-filter";
 import TaskList from "../core-components/task-list";
 import TaskSummary from "../core-components/task-summary";
 
+import type { filterProp } from "../core-components/task-filter";
 
 export default function PageComponents() {
-    return <>
-        <Search />
-        <TaskFilter />
-        <TaskList />
-        <TaskSummary />
-    </>
+
+    const [taskFilter, setTaskFilter] = useState<filterProp>('all')
+
+    return (
+        <>
+            <Search />
+
+            <TaskFilter
+                filter={taskFilter}
+                setFilter={setTaskFilter}
+            />
+
+            <TaskList
+                filter={taskFilter}
+            />
+
+            <TaskSummary />
+        </>
+    )
 }
